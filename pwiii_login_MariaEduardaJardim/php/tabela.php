7439e8e49e419cf33cf19e1cc8b1edd6cdf88623
Duda Jardim <maria.sousa437@etec.sp.gov.br>
2026-06-23 10:10:29 -0300

    Add files via upload

--- pwiii_login_MariaEduardaJardim/php/tabela.php
+++ pwiii_login_MariaEduardaJardim/php/tabela.php
@@ -1,42 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+<style>
+    .fundo{
+        background: #f0f0f0;
+    }
+</style>
+
+</head>
+
+<body class="container fundo">
+    
+</body>
+</html>
+
 <?php
-require 'Usuario.class.php';
+require '../php.Usuario.class.php';
 $usuario = new Usuario();
+$con = $usuario->conecta();
 
-$con = $usuario -> conecta();
+if (!$con){
+    echo "Banco indisponível. Tente mais tarde!";
+    exit();
+} else {
+    echo "<a href = '../html/cadastroUsuario.html' class = 'btn btn-sucess my-5'> Novo Usuário</a></p>";
 
-if($con){
-    $user = $usuario->listarUsuarios();
-    if(empty($user)){
-        echo "Não há usuários para listar!";
-    } else {
-        ?>
-        <table>
-            <tr>
-                <th>Código</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th coldspan = "2">Ações</th>
-            </tr>
-            <?php
-            foreach($user as $item){
-                $id = $item['id'];
-                $nome = $item['nome'];
-                $email = $item['email'];
-                ?>
+    $usuarios = $usuario->listarUsuarios();
 
-                <tr>
-                    <td><?php echo $item['id'];    ?></td>
-                    <td><?php echo $item['nome'];  ?></td>
-                    <td><?php echo $item['email']; ?></td>
-                    <td><a href= "editar.php?id=<?php echo $item['id'];  ?>">Editar</a></td>
-                    <td><a href= "excluir.php?id=<?php echo $item['id']; ?>">Excluir</a></td>
-                </tr>
-                <?php
-        }
-        ?>
-        </table>
-        <?php
+    //montagem do html da tabela
+
+    $table = '<table class = "table table-striped">';
+    $table .= '<thead>';
+    $table .= '<tr>';
+    $table .= '<th>Selecionar Usuários</th>';
+    $table .= '<th>Codigo</th>';
+    $table .= '<th>Nome</th>';
+    $table .= '<th>Email</th>';
+    $table .= '<th>Ações</th>';
+    $table .= '<tr>';
+    $table .= '<tbody>';
+
+
+    //laço de repetição para inclusão dos dados na tabela
+
+    foreach ($usuarios as $item){
+        $id = $item['id'];
+        $nome = ['nome'];
+        $email = ['email'];
+
+        $table .= '<tr>';
+        $table .= "<td><input type = 'checkbox' value = $id></td>";
+        $table .= "<td>$id</td>";
+        $table .= "<td>$nome</td>";
+        $table .= "<td>$email</td>";
+        $table .= "<td><a class = 'btn btn-info' href = 'editar.php?codigo=$id'>Editar</a></td>";
+        $table .= "<td><a class = 'btn btn-danger' href = 'deletar.php?id=$id'>Excluir</a><td>";
+
+        $table .= '</tr>';
+    }
+
+    $table .= '</tbody>';
+    $table .= '</thead>';
+    $table .= '</table>';
 }
-}else{
-    echo "Banco Indisponível. Tente mais tarde.";
-}
+
+echo $table;
+?>
